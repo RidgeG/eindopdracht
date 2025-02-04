fb5aef7a162a1f701651105164c91ce3f40f873a
RidgeG <ridgegeervliet@gmail.com>
2025-02-04 15:13:57 +0100

    pagina opstelling klaar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import AuthContextProvider from './contexts/AuthContext.jsx';
+import ThemeContextProvider from './contexts/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <Router>
+            <AuthContextProvider>
+                <ThemeContextProvider>
+                    <App />
+                </ThemeContextProvider>
+            </AuthContextProvider>
+        </Router>
+    </StrictMode>
+);
